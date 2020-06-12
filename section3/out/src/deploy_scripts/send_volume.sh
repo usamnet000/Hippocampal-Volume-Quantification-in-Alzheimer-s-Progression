5ed4920a372057660f0efac02d767e8255aeefb2
iDataist <hwdgrmy@gmail.com>
2020-06-11 19:28:42 -0500

    Update send_volume.sh

--- section3/out/src/deploy_scripts/send_volume.sh
+++ section3/out/src/deploy_scripts/send_volume.sh
@@ -4,8 +4,9 @@
 
 # In your test data directory you will find three different studies - you may change the dir here
 # to try all three out
-
+#cp -r /data/TestVolumes /home/workspace/
+# chmod -R 777 ./
 # apt-get install dcmtk
 # deploy_scripts/send_volume.sh
 # python3 inference_dcm.py '../TestVolumes'
-storescu 127.0.0.1 4242 -v -aec HIPPOAI +r +sd /data/TestVolumes/Study1
+storescu 127.0.0.1 4242 -v -aec HIPPOAI +r +sd /home/workspace/out/report.dcm #/data/TestVolumes/Study1
